27c5e41eb0b701b480c232942a9f5bb42f06ded5
AbdullahAhmed11 <abdullh442ahmed2@gmail.com>
2021-10-07 15:24:03 +0200

    deploy tesla web

---
+++ src/component/Home.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from 'styled-components';
+import Section from './Section';
+
+const Container = styled.div `
+height:100vh;
+`
+
+function Home() {
+    return (
+        <Container>
+            <Section
+            title="Model S"
+            description="Order Online For Touchless Delivery"
+            backgroundImg="model-s.jpg"
+            leftBtnText = "Custom Order"
+            rightBtnText = "Existing Inventory"
+            />
+            <Section
+            title="Model Y"
+            description="Order Online For Touchless Delivery"
+            backgroundImg="model-y.jpg"
+            leftBtnText = "Custom Order"
+            rightBtnText = "Existing Inventory"
+            />
+            <Section
+            title="Model 3"
+            description="Order Online For Touchless Delivery"
+            backgroundImg="model-3.jpg"
+            leftBtnText = "Custom Order"
+            rightBtnText = "Existing Inventory"
+            />
+            <Section
+            title="Model X"
+            description="Order Online For Touchless Delivery"
+            backgroundImg="model-x.jpg"
+            leftBtnText = "Custom Order"
+            rightBtnText = "Existing Inventory"
+            /> 
+            <Section
+            title="Lowest Cost Solar Panels In America"
+            description="Money-back guarantee"
+            backgroundImg="solar-panel.jpg"
+            leftBtnText = "Custom Order"
+            rightBtnText = "Learn more"
+            /> 
+            <Section
+            title="Lowest For New Roofs"
+            description="Money-back guarantee"
+            backgroundImg="solar-roof.jpg" 
+            leftBtnText = "Custom Order"
+            rightBtnText = "Learn more"
+            /> 
+            <Section
+            title="Accessories"
+            description="Money-back guarantee"
+            backgroundImg="accessories.jpg" 
+            leftBtnText = "Shop now"
+            /> 
+        </Container>
+    )
+}
+
+export default Home
